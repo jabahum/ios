16cd664a8be760c39c6ebf41b3630831ca2b5431
codecatalystlabs <codecatalystug@gmail.com>
2026-03-23 14:00:37 +0300

    swagger

--- cmd/web/docs/docs.go
+++ cmd/web/docs/docs.go
@@ -3160,7 +3160,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "response.health.go.ug",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Integrated Outbreak Surveillance (IOS) API",
